5bd583b3f14cc4bf8262d142099d8ad79bd78a0c
anatorrico <anatorricotirado@gmail.com>
2023-11-14 20:50:28 +0100

    whatwedo carusel

--- js/script.js
+++ js/script.js
@@ -4,3 +4,10 @@ const carousel = new bootstrap.Carousel(myCarouselElement, {
   interval: 1000,
   touch: false
 })
+
+const myCarouselElement1 = document.querySelector('#carouselAutoplaying1')
+
+const carousel1 = new bootstrap.Carousel(myCarouselElement, {
+  interval: 3000,
+  touch: false
+})
